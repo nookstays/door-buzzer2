21e8ea0d4cd28e6cbd732cb0c2e758c56584c587
nookstays <info@nookstays.co>
2025-11-08 14:56:36 +0100

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,20 @@
+{\rtf1\ansi\ansicpg1250\cocoartf2757
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 public-door-buzzer/\
+\uc0\u9500 \u9472 \u9472  index.html\
+\uc0\u9500 \u9472 \u9472  index.tsx\
+\uc0\u9500 \u9472 \u9472  App.tsx\
+\uc0\u9500 \u9472 \u9472  metadata.json\
+\uc0\u9500 \u9472 \u9472  constants.ts\
+\uc0\u9492 \u9472 \u9472  components/\
+    \uc0\u9500 \u9472 \u9472  BuzzerButton.tsx\
+    \uc0\u9492 \u9472 \u9472  icons/\
+        \uc0\u9500 \u9472 \u9472  ErrorIcon.tsx\
+        \uc0\u9500 \u9472 \u9472  LockClosedIcon.tsx\
+        \uc0\u9500 \u9472 \u9472  LockOpenIcon.tsx\
+        \uc0\u9492 \u9472 \u9472  SpinnerIcon.tsx}
